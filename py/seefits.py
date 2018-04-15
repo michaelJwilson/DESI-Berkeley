f6aeb775d382a051b1737d1801cebd09570d2bd7
Michael J. Wilson <michaeljwilson@lbl.gov>
2018-04-15 16:12:07 -0700

    Cleaned up seefits.py; Now complete.

--- py/seefits.py
+++ py/seefits.py
@@ -2,7 +2,7 @@ from    astropy.io   import  fits
 
 import  numpy        as      np
 import  collections
-import  glob
+import  sys
 import  os
 
 
@@ -17,6 +17,9 @@ roots      = {'DMOCKS': DMOCKS, 'CHALLENGE': CHALLENGE, 'DR12': DR12}
 
 
 def get_filenames(printit = True):
+  ## From ../infiles.txt, read the relative and absolute path for 
+  ## available keys.
+
   for line in infiles:
     if not line.strip().startswith("#"):
       [name, root, path]   = line.split() 
@@ -29,45 +32,36 @@ def get_filenames(printit = True):
   return allfits
 
 def print_header(allfits, name, printhead = False, tile = '00098'):  
+  ## Given a dictionary allfits, with each key corresponding to a .fits file
+  ## print out the available headers and the a number of instances as columns. 
+
   fname                    = allfits[name]
+
+  ## Currently hard coded fibers file;                                                                                                                      
+  if name == "FIBERS":
+    fname                  = "/project/projectdirs/desi/datachallenge/dc17b/fiberassign/tile_%s.fits" % tile
+  
+  print("\nGetting:  %s\n" % fname)
   
   try:
     dat                    =  fits.open(fname)
 
   except:
-    if name == "FIBERS":
-      fname                = "/project/projectdirs/desi/datachallenge/dc17b/fiberassign/tile_%s.fits" % tile
-      dat                  = fits.open(fname)
-
-    elif name == "TILES":
-      print("\n\nLoading tiles.\n\n")
-
-    else:
-      exit(1)
-
+      raiseValueError("Provided key was not available in provided dictionary of .fits files.")
+  
   hdrs                     = [x.header for x in dat]
   hdrkeys                  = list(dat[1].header.keys()) 
   
-  print("\nGetting:  %s\n" % fname)
-
   if printhead:
     print(repr(dat[1].header))
-
+  
   for i, x in enumerate(hdrkeys):
     if "TTYPE" in x:
       hdr = dat[1].header[i]
 
       if "COEFF" not in hdr:
         print "".join(np.str(x).ljust(25) for x in [i, hdr, dat[1].data[0][hdr], dat[1].data[1][hdr], dat[1].data[2][hdr], dat[1].data[3][hdr]])
-
-'''
-Available keys:  
-Input:      ELG, LRG, QSO, Lya
-Imaging:    TARGETS, TARGETSTRUTH, FIBERS, CONDITIONS, IN2OUT
-DR12:       DR12SDATA, DR12SRAND
-DarkSky:    DSELG, DSLRG, DSQSO, DSRAN
-Pointings:  TILES
-'''
+  
 
 if __name__ == "__main__":
   print("\n\nWelcome to seefits.\n\n")
@@ -77,19 +71,30 @@ if __name__ == "__main__":
 
   print("Available keys:  ")
 
+  '''                                                                                                                                                       
+  Input:      ELG, LRG, QSO, Lya                                                                                                                            
+  Imaging:    TARGETS, TARGETSTRUTH, FIBERS, CONDITIONS, IN2OUT                                                                                             
+  DR12:       DR12SDATA, DR12SRAND                                                                                                                             DarkSky:    DSELG, DSLRG, DSQSO, DSRAN                                                                                                                       Pointings:  TILES                                                                                                                                          
+  '''
+
   for x in allfits.keys():
     print x
 
-  name   = None
+  name   = ""
 
   while name is not "Exit":
-    name = input("\n\nEnter the key for your chosen file:\n")
-    name = str(name)
+    name = raw_input("\n\nEnter the key for your chosen file or Exit to finish:\n")
 
-    try: 
-      print_header(allfits, name)
+    if name == "Exit":
+      print("\n\nDone.\n\n")
 
-    except:
-      raiseValueError("\n\nChosen key is not available.")
+      sys.exit(0)
 
-  print("\n\nDone.\n\n")
+    if name.endswith('.fits'):
+      ## If user input ends in .fits, assume user is providing absolute path to a fits file. 
+      ## Write headers of this file to screen.
+      allfits['USER'] = name
+
+      name            = 'USER'
+
+    print_header(allfits, name)
